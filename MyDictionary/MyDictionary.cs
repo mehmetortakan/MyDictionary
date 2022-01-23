f10112bd9dd1164d0ca037b7419ccc12d86c51ef
Mehmet Ortakan <mehmetortakan@outlook.com>
2022-01-23 22:10:31 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _arrayKey;
+        TKey[] _tempArrayKey;
+        TValue[] _arrayValue;
+        TValue[] _tempArrayValue;
+
+        public MyDictionary()
+        {
+            _arrayKey = new TKey[0];
+            _arrayValue = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+
+        {
+            _tempArrayKey = _arrayKey;
+            _tempArrayValue = _arrayValue;
+            _arrayKey = new TKey[_arrayKey.Length + 1];
+            _arrayValue = new TValue[_arrayValue.Length + 1];
+
+            for (int i = 0; i < _tempArrayKey.Length; i++)
+            {
+                _arrayKey[i] = _tempArrayKey[i];
+
+            }
+            for (int j = 0; j < _tempArrayValue.Length; j++)
+            {
+                _arrayValue[j] = _tempArrayValue[j];
+            }
+
+            _arrayKey[_arrayKey.Length - 1] = key;
+            _arrayValue[_arrayKey.Length -1] = value;
+
+        }
+
+
+
+     
+
+        public int CountKey
+        {
+            get { return _arrayKey.Length;}
+            
+        }
+
+        public int CountValue
+        {
+            get { return _arrayValue.Length; }
+
+
+        }
+
+       
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MyDictionary;
+
+Console.WriteLine("---------------------STANDARD DICTIONARY--------------------------------");
+
+
+Dictionary<int,string> sehirler = new Dictionary<int,string>();
+
+sehirler.Add(1, "Ankara");
+sehirler.Add(2, "İstanbul");
+sehirler.Add(3, "İzmir");
+sehirler.Add(4, "Kahraman Maraş");
+
+
+foreach (var item in sehirler)
+{
+    
+    Console.WriteLine(item.Key+" "+item.Value);
+}
+
+Console.WriteLine(sehirler.Count);
+
+Console.WriteLine("--------------------------MY DICTIONARY---------------------------------");
+
+
+MyDictionary<int, string> cities = new MyDictionary<int, string>();
+
+cities.Add(1, "Ankara");
+cities.Add(2, "İstanbul");
+cities.Add(3, "İstanbul");
+cities.Add(4, "İstanbul");
+cities.Add(5, "İstanbul");
+
+Console.WriteLine(cities.CountKey);
+Console.WriteLine(cities.CountValue);
+
+
+
+
